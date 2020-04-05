568104ad670d93e1ce560fac72311ab050264e58
Hongbin Li <hblee12294@gmail.com>
2020-04-04 19:14:15 -0700

    build: update build

---
+++ precache-manifest.892bdb55438d5a7ec170742b19b4ff84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179792518eb285e219b1c634d0a7bb81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9802694b26715c4461cf",
+    "url": "/static/css/main.29fa5366.chunk.css"
+  },
+  {
+    "revision": "6e14ac1d918fa7a91429",
+    "url": "/static/js/2.4269edfc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4269edfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9802694b26715c4461cf",
+    "url": "/static/js/main.ad19e09e.chunk.js"
+  },
+  {
+    "revision": "fe891463aef95e79cf5e",
+    "url": "/static/js/runtime-main.a96114f2.js"
+  },
+  {
+    "revision": "bcfb9394f4141ab0c9cdaeaf62f1aadd",
+    "url": "/static/media/water.bcfb9394.jpg"
+  }
+]);
